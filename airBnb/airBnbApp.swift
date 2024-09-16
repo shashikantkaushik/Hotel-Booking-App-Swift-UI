d4a271dcdd965b0c1f11cf58d61c95b7eece21c3
Shashi Kant <shashikantkaushik4@gmail.com>
2024-09-16 22:25:25 +0530

    Initial Commit

---
+++ airBnb/airBnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  airBnbApp.swift
+//  airBnb
+//
+//  Created by Shashi Kant on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct airBnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
